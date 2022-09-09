ed942a522ea6785fe9a5301cc4894e902ee53635
Wallace Vieira <87558176+wallarock@users.noreply.github.com>
2022-09-08 22:13:57 -0300

    teste pos

---
+++ Teste pos/src/teste/da/pos/Pos.java
@@ -0,0 +1,17 @@
+package teste.da.pos;
+
+public class Pos {
+
+	public static void main(String[] args) {
+		
+			int num=1, soma=0;
+			while (num <= 100) {
+			soma += num;
+			num++;
+			}
+			System.out.println("Soma dos números entre 1 e 100 = "+soma);
+			 
+	}
+
+}
+ 
