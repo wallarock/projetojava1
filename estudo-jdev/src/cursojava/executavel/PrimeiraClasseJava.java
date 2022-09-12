ac77601db9c799e423d4df9beaeb25b2afb6fec5
Wallace Vieira <87558176+wallarock@users.noreply.github.com>
2022-09-11 21:51:18 -0300

    Estudo 11/09/2022

--- estudo-jdev/src/cursojava/executavel/PrimeiraClasseJava.java
+++ estudo-jdev/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,25 +1,38 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 import cursojava.classes.Aluno;
+import cursojava.classes.Disciplina;
 
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 		
+		String nome = JOptionPane.showInputDialog("Nome do aluno: ");
+		String idade = JOptionPane.showInputDialog("idade: ");
+		
 		Aluno aluno1 = new Aluno();
-		Aluno aluno2 = new Aluno();
+				
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade));
 		
-		Aluno aluno3 = new Aluno("Maria");
+		for (int pos = 1; pos <= 3; pos++) {
+			
+			String nomeDisciplina = JOptionPane.showInputDialog("Qual é o nome da disciplina "+pos+" ?: ");
+			String notaDisciplina = JOptionPane.showInputDialog("Qual a nota da disciplina " +pos+"?: ");
+			
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(nomeDisciplina);
+			disciplina.setNota(Double.valueOf(notaDisciplina));
+			
+			aluno1.getDisciplinas().add(disciplina);
+		}
 		
-		aluno1.setNome("Wallace");
-		aluno1.setDataNascimento("10/10/1983");
-		aluno1.setNota1(90);
-		aluno1.setNota2(80);
-		aluno1.setNota3(70);
-		aluno1.setNota4(85);
-		
+				
 		System.out.println("O nome é: " + aluno1.getNome());
-		System.out.println("A data de nascimento é: " + aluno1.getDataNascimento());
+		System.out.println("A Idade é: " + aluno1.getIdade());
+		System.out.println("Nome das disciplinas: " + aluno1.getDisciplinas());
 		System.out.println("A média da nota é: " + aluno1.getMediaNota());
 		System.out.println("Resultado: " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
 		
